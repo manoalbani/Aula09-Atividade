d3a6ff6deb89636b7ec8ab35ac72e33075c77925
André Manoel Albani Cezar <andre.11952@sesc-sc.com.br>
2023-05-18 10:47:52 -0300

    atividade

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,68 @@ import './App.css'
 
 function App() {
   let [livros, setLivros] = useState([]);
-  let[livro, setLivro] = useState({});
+  let[nomeLivro, setNomeLivro] = useState('');
+  let[codigoLivro, setCodigoLivro] = useState('');
+  let[autorLivro, setAutorLivro] = useState('');
 
   function addLivro(){
-    console.log(livro);
+    let livro = {
+      codigo: codigoLivro,
+      nome:nomeLivro,
+      autor: autorLivro,
+    }
     livros.push(livro);
     setLivros([...livros]);
-    setLivro({});
-    console.log(livro);
+    limparForm();
   }
+  function limparForm(){
+    setNomeLivro('');
+    setCodigoLivro('');
+    setAutorLivro('');
+  }
+
+  function removerLivro(livro){
+    console.log(livro);
+    livros.splice(livros.indexOf(livro), 1);
+    setLivros([...livros]);
+  }
+
   return (
     <div>
    <input 
-    value={livro.nome}
-    onChange={(e)=>{ livro.nome = e.target.value;
-      setLivro(livro)}}
-
+   placeholder='Código'
+    value={codigoLivro}
+    onChange={(e)=>{ setCodigoLivro(e.target.value)}}
+   />
+   <input 
+    placeholder='Nome Livro'
+    value={nomeLivro}
+    onChange={(e)=>{ setNomeLivro(e.target.value)}}
+   />
+   <input 
+    placeholder='Autor'
+    value={autorLivro}
+    onChange={(e)=>{ setAutorLivro(e.target.value)}}
    />
    <button onClick={addLivro}>Salvar</button>
+   <div className='livros'>
+   <table>
+   <tr>
+      <th >Código</th>
+        <th >Nome</th>
+        <th >Autor</th>
+    </tr>
+    {livros.map(x=>{
+      return       <tr>
+        <th >{x.codigo}</th>
+        <th >{x.nome}</th>
+        <th >{x.autor}</th>
+        <th ><button onClick={()=>{removerLivro(x)}} >Remover</button></th>
+        
+      </tr>
+    })}
+   </table>
+   </div>
    </div>
   )
 }
