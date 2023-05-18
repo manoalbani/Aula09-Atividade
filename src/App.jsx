3e9eca4ee2c0a9cbf84ebe9bab473bcf9a9c4fb6
André Manoel Albani Cezar <andre.11952@sesc-sc.com.br>
2023-05-17 22:34:45 -0300

    Dev Atividade

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,26 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  let [livros, setLivros] = useState([]);
+  let[livro, setLivro] = useState({});
 
+  function addLivro(){
+    console.log(livro);
+    livros.push(livro);
+    setLivros([...livros]);
+    setLivro({});
+    console.log(livro);
+  }
   return (
-   <h1>Hello</h1>
+    <div>
+   <input 
+    value={livro.nome}
+    onChange={(e)=>{ livro.nome = e.target.value;
+      setLivro(livro)}}
+
+   />
+   <button onClick={addLivro}>Salvar</button>
+   </div>
   )
 }
 
